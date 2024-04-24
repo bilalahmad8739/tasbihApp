3a53c3a1e388aa12601bffcec3f17e0b54a47833
Bilal Ahmad <94444468+bilalahmad8739@users.noreply.github.com>
2024-04-24 12:54:31 +0500

    .

--- lib/core/getx/homescreenget.dart
+++ lib/core/getx/homescreenget.dart
@@ -1,12 +1,35 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreenController extends GetxController {
   var count = 0.obs;
-  void loadprefrences() async {
-    final prefs = await SharedPreferences.getInstance();
+  final List<Color> _colorOptions = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+    Colors.orange,
+    Colors.purple,
+  ].obs;
+  Rx<Color> containerColor = Color.fromARGB(255, 25, 30, 34).obs;
 
-      count.value  = (prefs.getInt('value') ?? 0);
+  RxInt currentColorIndex = 0.obs;
+  List<bool> isTabTapped = [false, false, false, false].obs;
+
+  void colorChange(int index) {
+    isTabTapped[index] = !isTabTapped[index];
+    //print("color is ${isTabTapped[index]}");
+  }
+
+  void loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    count.value = prefs.getInt('value') ?? 0;
+    final savedColorIndex = prefs.getInt('colorIndex') ?? 0;
+    currentColorIndex.value = savedColorIndex;
+    containerColor.value = _colorOptions[savedColorIndex];
   }
 
   void incrementCounter() async {
@@ -23,5 +46,16 @@ class HomeScreenController extends GetxController {
     if (count.value > 0) {
       count--;
     }
+  }
+
+  void colorChangeBackground() async {
+    // Increment the current color index
+    currentColorIndex.value =
+        ((currentColorIndex.value + 1) % _colorOptions.length);
+    // Set the container color to the next color in the list
+    containerColor.value = _colorOptions[currentColorIndex.value];
+    // Save the selected color index to SharedPreferences
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('colorIndex', currentColorIndex.value);
   }
 }
--- lib/presentation/UI/homescreen/homescreen.dart
+++ lib/presentation/UI/homescreen/homescreen.dart
@@ -10,250 +10,222 @@ import 'package:vibration/vibration.dart';
 // import 'package:vibration/vibration.dart';
 
 class CounterScreen extends StatefulWidget {
+  final HomeScreenController homeScreenController = Get.put(HomeScreenController());
+
   @override
   _CounterScreenState createState() => _CounterScreenState();
 }
 
 class _CounterScreenState extends State<CounterScreen> {
   // bool istap = false;
-  List<bool> isTabTapped = [false, false, false, false];
-  final List<Color> _colorOptions = [
-    Colors.red,
-    Colors.blue,
-    Colors.green,
-    Colors.yellow,
-    Colors.orange,
-    Colors.purple,
-  ];
-  Color _containerColor = const Color.fromARGB(255, 25, 30, 34);
-  int _currentColorIndex = 0;
-  void colorChangeBackground() {
-    setState(() {
-      // Increment the current color index
-      _currentColorIndex = (_currentColorIndex + 1) % _colorOptions.length;
-      // Set the container color to the next color in the list
-      _containerColor = _colorOptions[_currentColorIndex];
-    });
-  }
-
-  void colorChange(int index) {
-    isTabTapped[index] = !isTabTapped[index];
-    print("color is ${isTabTapped[index]}");
-  }
 
   @override
   void initState() {
     // TODO: implement initState
     // count;
-    _currentColorIndex;
-    _controller.loadprefrences();
+    widget.homeScreenController.currentColorIndex.value;
+    widget.homeScreenController.loadPreferences();
     super.initState();
   }
 
-  final HomeScreenController _controller = Get.put(HomeScreenController());
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: _containerColor,
-      appBar: AppBar(
-        title: const Text('Tasbih Lite'),
-        centerTitle: true,
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-            );
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.info),
-            onPressed: () {
-              // Add your action for the right-side icon
+ 
+    return Obx(
+      () => 
+      Scaffold(
+        backgroundColor: widget.homeScreenController.containerColor.value,
+        appBar: AppBar(
+          title: const Text('Tasbih Lite'),
+          centerTitle: true,
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
             },
           ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Add your action for Drawer item 1
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.info),
+              onPressed: () {
+                // Add your action for the right-side icon
               },
             ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Add your action for Drawer item 2
-              },
-            ),
-            // Add more Drawer items as needed
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 70),
-            child: Container(
-              height: 300,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                // color: _containerColor,
-
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.centerRight,
-                    height: 60,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5),
-                      child: Obx(
-                        () {
-                          return Text(
-                            '${_controller.count}',
-                            style: const TextStyle(fontSize: 50.0),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            _controller.removeOneCount();
-                          },
-                          child: Container(
-                            height: 50,
-                            width: 50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                            ),
-                            child: const Icon(Icons.replay),
-                          ),
-                        ),
-                        const SizedBox(width: 16.0),
-                        InkWell(
-                          onTap: () {
-                            _controller.resetCounter();
-                          },
-                          child: Container(
-                            height: 50,
-                            width: 50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                            ),
-                            child: const Icon(Icons.restart_alt_sharp),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () async {
-                      _controller.incrementCounter();
-                    },
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 250),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      _vibrate();
-                      colorChange(0);
-                    });
-                  },
-                  child: tabbarContainer(
-                    istap: isTabTapped[0],
-                    icon: Icons.vibration_outlined,
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
                   ),
                 ),
-                InkWell(
-                    onTap: () {
-                      setState(() {
-                        colorChange(1);
-                      });
-                    },
-                    child: tabbarContainer(
-                      istap: isTabTapped[1],
-                      icon: Icons.vibration,
-                    )),
-                InkWell(
-                    onTap: () {
-                      setState(() {
-                        colorChange(2);
-                      });
-                    },
-                    child: tabbarContainer(
-                      istap: isTabTapped[2],
-                      icon: Icons.countertops,
-                    )),
-                InkWell(
-                    onTap: () {
-                      colorChangeBackground();
-                      setState(() {
-                        colorChange(3);
-                      });
-                    },
-                    child: tabbarContainer(
-                      istap: isTabTapped[3],
-                      icon: Icons.invert_colors,
-                    )),
-              ],
-            ),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {},
+              ),
+            ],
           ),
-        ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 70),
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  // color: _containerColor,
+      
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.centerRight,
+                      height: 60,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Obx(
+                          () {
+                            return Text(
+                              '${widget.homeScreenController.count}',
+                              style: const TextStyle(fontSize: 50.0),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50, vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              widget.homeScreenController.removeOneCount();
+                            },
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: const Icon(Icons.replay),
+                            ),
+                          ),
+                          const SizedBox(width: 16.0),
+                          InkWell(
+                            onTap: () {
+                              widget.homeScreenController.resetCounter();
+                            },
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: const Icon(Icons.restart_alt_sharp),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () async {
+                        widget.homeScreenController.incrementCounter();
+                      },
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 250),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          _vibrate();
+                          widget.homeScreenController.colorChange(0);
+                        },
+                        child: tabbarContainer(
+                          istap: widget.homeScreenController.isTabTapped[0],
+                          icon: Icons.vibration_outlined,
+                        ),
+                      ),
+                      InkWell(
+                          onTap: () {
+                            widget.homeScreenController.colorChange(1);
+                          },
+                          child: tabbarContainer(
+                            istap: widget.homeScreenController.isTabTapped[1],
+                            icon: Icons.vibration,
+                          )),
+                      InkWell(
+                          onTap: () {
+                            widget.homeScreenController.colorChange(2);
+                          },
+                          child: tabbarContainer(
+                            istap: widget.homeScreenController.isTabTapped[2],
+                            icon: Icons.countertops,
+                          )),
+                      InkWell(
+                          onTap: () {
+                           
+                              widget.homeScreenController.colorChangeBackground();
+      
+                            widget.homeScreenController.colorChange(3);
+                         
+                          },
+                          child: tabbarContainer(
+                            istap: widget.homeScreenController.isTabTapped[3],
+                            icon: Icons.invert_colors,
+                          )),
+                    ],
+                  ))
+                
+            
+          ],
+        ),
       ),
     );
   }
